84fd43f9b7ab30d54dc1e2aa9c104826974e1806
Chris Mang <cmangkuo@calpoly.edu>
2014-04-03 15:01:28 -0700

    track Config script and pledge

---
+++ ConfigureGit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global user.name "Chris Mang"
+git config --global user.email "cmangkuo@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
